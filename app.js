958b6f836c4431747862188d9a9cc2234fbe546c
valost <valerieostrovska@yahoo.com>
2023-08-06 18:58:55 +0300

    added

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hi');
+// console.log('Hi');
+
+// for (let myAge = 31; myAge <=35; myAge++) {
+//     console.log(`I am ${myAge}`);
+// }
+
+const n = 10;
+let sum = 0;
+
+
+
+for (let i = 1; 1 <= n; i++) {
+    sum += i;
+}
+
+console.log(sum)
